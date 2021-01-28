130ec3215893cabb3babb4937c0184b8c13f9385
Aniruddha Pandey <anirudh.pandev@gmail.com>
2021-01-28 06:51:34 +0530

    profile screen should work

--- Views/Profile/index.js
+++ Views/Profile/index.js
@@ -1,16 +1,19 @@
 import React, { useState, useEffect, useContext } from 'react'
-import { Text, View, Button } from 'react-native'
+import { Text, View, Button, StyleSheet, ScrollView, TextInput } from 'react-native'
 import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack'
 import auth from '@react-native-firebase/auth'
 import { Details, SignIn, SignUp } from "Views"
 import AppContext from "context/app-context.js"
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import { Picker } from '@react-native-picker/picker'
 
 const Stack = createStackNavigator()
 
 const Profile = ({ navigation }) => {
 
   const { initializingFirebase, userState } = useContext(AppContext)
+
+  const [mets, setMets] = useState("6.8")
 
   if (initializingFirebase) return null
 
@@ -34,10 +37,76 @@ const Profile = ({ navigation }) => {
   return (
     <>
     { userState 
-      ? <View>
-          <Text>Welcome {userState.email}</Text>
-          <Details />
-          <Button title="sign out" onPress={() => signOut()} />
+      ? <View style={styles.container}>
+          <View style={styles.heading}>
+            <Text style={{...styles.title, color: "black"}}>Welcome, <Text style={styles.title}>{userState.email}</Text></Text>
+          </View>
+          <Button color="#1873FF" title="sign out" onPress={() => signOut()} />
+          <ScrollView style={styles.form}>
+            <View style={styles.field}>
+              <Text style={styles.label}>Name</Text>
+              <TextInput
+                style={{...styles.input, borderColor: "#f2f2f2"}}
+                value={"value"}
+                editable={false}
+              />
+            </View>
+            <View style={styles.field}>
+              <Text style={styles.label}>Age</Text>
+              <View style={styles.specialInput}>
+                <TextInput
+                  style={{...styles.input, borderColor: "#f2f2f2"}}
+                  value={"value"}
+                  editable={false}
+                />
+                <TextInput defaultValue="yr" editable={false} />
+              </View>
+            </View>
+            <View style={styles.field}>
+              <Text style={styles.label}>Sex</Text>
+              <Picker
+                selectedValue={"userSex"}
+                style={{height: 50}}
+                enabled={false}>
+                <Picker.Item label="Male" value="male" />
+                <Picker.Item label="Female" value="female" />
+              </Picker>
+            </View>
+            <View style={styles.field}>
+              <Text style={styles.label}>Height</Text>
+              <View style={styles.specialInput}>
+                <TextInput
+                  style={{...styles.input, borderColor: "#f2f2f2"}}
+                  value={"value"}
+                  editable={false}
+                />
+                <TextInput defaultValue="cm" editable={false} />
+              </View>
+            </View>
+            <View style={styles.field}>
+              <Text style={styles.label}>Weight</Text>
+              <View style={styles.specialInput}>
+                <TextInput
+                  style={{...styles.input, borderColor: "#f2f2f2"}}
+                  value={"value"}
+                  editable={false}
+                />
+                <TextInput defaultValue="kg" editable={false} />
+              </View>
+            </View>
+            <View style={{...styles.field, paddingBottom: 50}}>
+              <Text style={styles.label}>Mets</Text>
+              <Picker
+                selectedValue={"6.8"}
+                style={{height: 50}}
+                onValueChange={value => setMets(value)}>
+                <Picker.Item label="bicycling, general" value="7.5" />
+                <Picker.Item label="bicycling, leisure" value="3.5" />
+                <Picker.Item label="bicycling, mountain, uphill, vigorous" value="14.0" />
+                <Picker.Item label="bicycling, mountain, general" value="8.5" />
+              </Picker>
+            </View>        
+          </ScrollView>
         </View>
       : <Stack.Navigator initialRouteName="SignIn" headerMode="none">
           <Stack.Screen name="SignIn" component={SignIn} />
@@ -47,5 +116,53 @@ const Profile = ({ navigation }) => {
     </>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+    fontFamily: "Roboto"
+  },
+  heading: {
+    padding: 50,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#E2EDFF"
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 50,
+    color: "#1873FF"
+  },
+  form: {
+    padding: 20,
+    paddingLeft: 30,
+    paddingRight: 30
+  },
+  field: {},
+  label: {
+    color: "black",
+    fontWeight: "bold",
+    paddingTop: 20,
+  },
+  specialInput: {
+    display: "flex",
+    flexDirection: "row"
+  },  
+  input: {
+    borderWidth: 1,
+    borderRadius: 5,
+    height: 40,
+    flex: 1
+  },
+  register: {
+    padding: 20,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  }
+})
 
 export default Profile
